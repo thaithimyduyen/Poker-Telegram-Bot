fb114ebd9fdffc643e9e1a2eb1fa2aeb2071f4a1
kkoemets <koemets.kristjan@gmail.com>
2023-04-08 14:28:48 +0300

    Add tests for /ready, /start

--- tests/test_pokerbotmodel.py
+++ tests/test_pokerbotmodel.py
@@ -24,22 +24,40 @@ class TestPokerBotModel(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestPokerBotModel, self).__init__(*args, **kwargs)
 
-        self._model = PokerBotModel(MagicMock(spec=PokerBotViewer), MagicMock(spec=Bot), MagicMock(spec=Config), None)
+        self._model: PokerBotModel = PokerBotModel(MagicMock(spec=PokerBotViewer), MagicMock(spec=Bot),
+                                                   MagicMock(spec=Config), None)
+        self._view = MagicMock(spec=PokerBotViewer)
+        self._view.text = ''
+        self._view.send_message_reply = lambda chat_id, message_id, text: setattr(self._view, 'text', text)
+        self._view.send_message = lambda chat_id, text, reply_markup=None: setattr(self._view, 'text', text)
+        self._model._view = self._view
+
+        self._kv = MagicMock(spec=dict)
+        self._kv.get = lambda key: 0
+        self._kv.inc_amount = 0
+        self._kv.incrby = lambda name, amount: setattr(self._kv, 'inc_amount', amount)
+        self._model._kv = self._kv
+
+        self._cfg = MagicMock(spec=Config)
+        self._cfg.DEBUG = False
+        self._model._cfg = self._cfg
+
+    @staticmethod
+    def _create_player(user_id: str, cards: []) -> Player:
+        player: Player = Player(user_id, user_id, MagicMock(spec=Wallet), '0')
+        player.user_id = user_id
+        player.test_amount = 0
+        player.wallet.inc = lambda amount: setattr(player, 'test_amount', player.test_amount + amount)
+        player.cards = cards
+        player.mention_markdown = user_id
+        return player
 
     def test_reset_game(self):
         game: Game = Game()
 
-        def create_player(user_id: str) -> Player:
-            player: Player = MagicMock(spec=Player)
-            player.user_id = user_id
-            player.wallet = MagicMock(spec=Wallet)
-            player.test_amount = 0
-            player.wallet.inc = lambda amount: setattr(player, 'test_amount', player.test_amount + amount)
-            return player
-
-        player_one: Player = create_player('1')
-        player_two: Player = create_player('2')
-        player_three: Player = create_player('3')
+        player_one: Player = self._create_player('1', [])
+        player_two: Player = self._create_player('2', [])
+        player_three: Player = self._create_player('3', [])
         game.players = [player_one, player_two, player_three]
 
         game.players_bets = [
@@ -63,18 +81,12 @@ class TestPokerBotModel(unittest.TestCase):
         self.assertEqual(10, player_three.test_amount)
 
     def test_create_final_result_text(self):
-        def create_player(mention_markdown, player_cards) -> Player:
-            player = MagicMock(spec=Player)
-            player.cards = [player_cards.pop(), player_cards.pop()]
-            player.mention_markdown = mention_markdown
-            return player
-
         cards: Cards = get_cards()
         cards.sort(key=lambda card: (card.rank, card.suit), reverse=True)
 
-        player_one: Player = create_player("Player 1", cards)
+        player_one: Player = self._create_player("Player 1", [cards.pop(), cards.pop()])
 
-        player_two: Player = create_player("Player 2", cards)
+        player_two: Player = self._create_player("Player 2", [cards.pop(), cards.pop()])
 
         game: Game = MagicMock(spec=Game)
         game.cards_table = [cards.pop(), cards.pop(), cards.pop(), cards.pop(), cards.pop()]
@@ -99,38 +111,22 @@ class TestPokerBotModel(unittest.TestCase):
     def test_top_up(self):
         model = self._model
 
-        kv = MagicMock(spec=dict)
-        kv.get = lambda key: 0
-        kv.topped_up_amount = 0
-        kv.incrby = lambda name, amount: setattr(kv, 'topped_up_amount', amount)
-        model._kv = kv
-
         model._game_from_context = lambda a: Game()
-
-        view = MagicMock(spec=PokerBotViewer)
-        model._view = view
-
-        view.text = ''
-        model._view.send_message_reply = lambda chat_id, message_id, text: setattr(view, 'text', text)
 
         update = MagicMock(spec=Update)
         context = MagicMock(spec=CallbackContext)
 
-        self.assertEqual('', view.text)
+        self.assertEqual('', model._view.text)
 
         model.top_up(update, context)
 
-        self.assertEqual(1000, kv.topped_up_amount)
-        self.assertEqual('Your wallet is topped up with 1000 $', view.text)
+        self.assertEqual(1000, model._kv.inc_amount)
+        self.assertEqual('Your wallet is topped up with 1000 $', model._view.text)
 
     def test_top_up__should_not_top_up_when_wallet_not_empty(self):
         model = self._model
 
-        kv = MagicMock(spec=dict)
-        kv.get = lambda key: 1
-        kv.topped_up_amount = 0
-        kv.incrby = lambda name, amount: setattr(kv, 'topped_up_amount', amount)
-        model._kv = kv
+        model._kv.get = lambda key: 1
 
         model._game_from_context = lambda a: Game()
 
@@ -147,38 +143,82 @@ class TestPokerBotModel(unittest.TestCase):
 
         model.top_up(update, context)
 
-        self.assertEqual(0, kv.topped_up_amount)
+        self.assertEqual(0, model._kv.inc_amount)
         self.assertEqual('Your wallet is not empty. You can not top up it.', view.text)
 
     def test_top_up__should_not_top_up_when_game_in_progress(self):
         model = self._model
-
-        kv = MagicMock(spec=dict)
-        kv.get = lambda key: 0
-        kv.topped_up_amount = 0
-        kv.incrby = lambda name, amount: setattr(kv, 'topped_up_amount', amount)
-        model._kv = kv
 
         game = Game()
         game.state = GameState.ROUND_PRE_FLOP
 
         model._game_from_context = lambda a: game
 
-        view = MagicMock(spec=PokerBotViewer)
-        model._view = view
-
-        view.text = ''
-        model._view.send_message_reply = lambda chat_id, message_id, text: setattr(view, 'text', text)
-
         update = MagicMock(spec=Update)
         context = MagicMock(spec=CallbackContext)
 
+        view = model._view
         self.assertEqual('', view.text)
 
         model.top_up(update, context)
 
-        self.assertEqual(0, kv.topped_up_amount)
+        self.assertEqual(0, model._kv.inc_amount)
         self.assertEqual('Game is in progress. You can not top up your wallet.', view.text)
+
+    def test_ready(self):
+        model = self._model
+
+        model._kv.get = lambda key: 1000
+
+        game = Game()
+        game.state = GameState.INITIAL
+
+        model._game_from_context = lambda context: game
+
+        update = MagicMock(spec=Update)
+        update.effective_message.from_user.id = '1'
+
+        self.assertEqual(model._view.text, '')
+        self.assertEqual(len(game.ready_users), 0)
+        self.assertEqual(len(game.players), 0)
+
+        model.ready(update, MagicMock(spec=CallbackContext))
+
+        self.assertTrue(model._view.text.startswith('You are ready now'),
+                        f"Actual text: {model._view.text}")
+
+        self.assertEqual(len(game.ready_users), 1)
+        self.assertEqual(game.ready_users.copy().pop(), '1')
+        self.assertEqual(len(game.players), 1)
+
+    def test_start(self):
+        model = self._model
+
+        game = Game()
+        model._game_from_context = lambda context: game
+
+        game.state = GameState.INITIAL
+
+        player_one = self._create_player('1', [])
+        player_one.wallet.value = lambda: 1000
+
+        player_two = self._create_player('2', [])
+        player_two.wallet.value = lambda: 1000
+
+        game.players = [player_one, player_two]
+
+        model._bot.get_chat_member_count = lambda chat_id: 3
+
+        context = MagicMock(spec=CallbackContext)
+        context.chat_data.get = lambda key, arr: []
+
+        self.assertEqual(model._view.text, '')
+
+        model._round_rate.finish_rate = lambda game, player_scores: 15
+
+        model.start(MagicMock(spec=Update), context)
+
+        self.assertEqual(model._view.text, 'The game is started! 🃏')
 
 if __name__ == '__main__':
     unittest.main()
